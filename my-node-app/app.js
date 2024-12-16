1ff348104627e4e51defb9fff9e91d7e0d8ffefd
AffanOnGit <affanhameed3096@gmail.com>
2024-12-16 17:25:40 +0500

    edited ajax file

---
+++ my-node-app/app.js
@@ -0,0 +1,3 @@
+const http = require('http'); //this means we are importing the http module
+const hostname = '127.0.0.1'; //this is the IP address of the server
+const port = 3000; //this is the port number of the server
